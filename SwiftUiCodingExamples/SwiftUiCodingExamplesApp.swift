ea2004cfe7ae10362aaa6e0832770ed9f7806f36
Johnny Hacking <jhacking@bhico.com>
2021-05-26 22:28:39 -0600

    Initial Commit

---
+++ SwiftUiCodingExamples/SwiftUiCodingExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCodingExamplesApp.swift
+//  SwiftUiCodingExamples
+//
+//  Created by Johnny Hacking on 5/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCodingExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
